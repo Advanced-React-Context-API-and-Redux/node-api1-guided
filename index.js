f9554b18878a84a1d6254625fa097a138cea039b
seanaleid <seanaleid@gmail.com>
2020-03-09 13:45:24 -0400

    Create a .post endpoint at /api/lessons
      * Adds a .post endpoint to receive lessons

--- index.js
+++ index.js
@@ -2,18 +2,47 @@
 // import isn't supported in node out of the box in every version, require is however, supported right out of the box
 // require existed before import existed
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 
-// server.get('/', (req, res) => {
-//     res.status(200).json({api: 'running...'})
-// })
+let hubs = [];
+let lessons = [];
+
+server.use(express.json()); // <<<<<<<<<<<<<<<<<< include ths line so the server can real json 
+
+server.get('/', (req, res) => {
+    res.status(200).json({api: 'running...'})
+})
 
 // write / hello endpoint that returns an object like: { hello: "WEB 27" }
-server.get('/', (req, res) => {
+server.get('/hello', (req, res) => {
     res.status(200).json({ hello: 'WEB 27'})
 })
 
+// create a .post endpoint (follow along)
+server.post('/api/hubs', (req, res) => {
+    const hubInfo = req.body;
+
+    // validate the hubInfo is correct before saving
+    hubInfo.id = shortid.generate();
+
+    hubs.push(hubInfo);
+
+    res.status(201).json(hubInfo);
+})
+
+// create a .post to receive lessons from /api/lessons/
+server.post('/api/lessons', (req, res) => {
+    const lessonInfo = req.body;
+
+    // validate the hubInfo is correct before saving
+    lessonInfo.id = shortid.generate();
+
+    lessons.push(lessonInfo);
+
+    res.status(201).json(lessonInfo);
+})
 
 const PORT = 5000;
 server.listen(PORT, () => console.log(`\n ** API on http://localhost:${PORT} ** \n`)
